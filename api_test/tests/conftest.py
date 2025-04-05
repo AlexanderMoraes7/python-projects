d683497d67385f521d6effb6d7564f31a53182e0
Alex <alexander97643@gmail.com>
2025-04-05 15:18:49 -0300

    insert testings and connection with database in docker

--- api_test/tests/conftest.py
+++ api_test/tests/conftest.py
@@ -1,9 +1,24 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from api_test.app import app
+from api_test.models import table_registry
+from api_test.settings import Settings
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine(Settings().DATABASE_URL)
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
